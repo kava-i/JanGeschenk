5b245a1801376626842b5df0f502cf4fa7e16531
Alexander Leonhardt <equinox.salexander@gmail.com>
2020-02-01 16:15:37 +0100

    kk

---
+++ cia.py
@@ -0,0 +1,7 @@
+import ciavelli
+
+class CiaPackage(ciavelli.Package):
+    def build():
+        print("Doing secret build right now!")
+    def install():
+        print("Doing secret install right now!")
