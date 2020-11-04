6adec40df8f821e067e113c017446b0934e7d512
fux <jan.dick@bdp.org>
2020-11-04 11:39:15 +0100

    Fixes serious error (not calling on close function)

--- Webconsole.hpp
+++ Webconsole.hpp
@@ -25,76 +25,66 @@ using websocketpp::lib::bind;
 typedef server::message_ptr message_ptr;
 
 enum class color {
-        white = 1,
-        red = 2,
-        green = 3,
-        yellow = 4,
-        blue = 5
+  white = 1,
+  red = 2,
+  green = 3,
+  yellow = 4,
+  blue = 5
 };
 
 class Webconsole
 {
-        private:
-                websocketpp::connection_hdl _conn;
-                server *s;
-                std::string _flushCont;
-        public:
-                Webconsole(server *sk,websocketpp::connection_hdl conn) : _conn(conn),s(sk)
-        {}
+  private:
+    websocketpp::connection_hdl _conn;
+    server *s;
+    std::string _flushCont;
+  public:
+    Webconsole(server *sk,websocketpp::connection_hdl conn) : _conn(conn),s(sk) {}
 
-                template<typename ...Args,typename T>
-                        void write(T s,Args... args)
-                        {
-                                write(s);		//Write the current param to screen
-                                write(args...);	//Recursively call ourself with the rest of the params, let the compiler decide which function to call
-                        }
+    template<typename ...Args,typename T>
+    void write(T s,Args... args) {
+      write(s);		//Write the current param to screen
+      write(args...);	//Recursively call ourself with the rest of the params, let the compiler decide which function to call
+    }
 
-                /**
-                 * @brief The recursion anchor for _write tries to convert the argument to string to print it with a easy print string function
-                 * @param argument The object to print
-                 */
-                template<typename T>
-                        void write(T argument)
-                        {
-                                write(std::to_string(argument));
-                        }
+    /**
+     * @brief The recursion anchor for _write tries to convert the argument to string to print it with a easy print string function
+     * @param argument The object to print
+     */
+    template<typename T>
+    void write(T argument) {
+      write(std::to_string(argument));
+    }
 
+    void write(std::string kk) {
+      _flushCont+=kk;
+    }
 
-                void write(std::string kk)
-                {
-                        _flushCont+=kk;
-                }
+    void write(const char *k) {
+      _flushCont+=k;
+    }
 
-                void write(const char *k)
-                {
-                        _flushCont+=k;
-                }
+    void write(int x) {
+      _flushCont+=std::to_string(x);
+    }
 
-                void write(int x)
-                {
-                        _flushCont+=std::to_string(x);
-                }
+    void write(color col) {
+      char c = 1;
+      switch(col) {
+        case color::white: c = 1;break;
+        case color::red: c = 2; break;
+        case color::green: c = 3; break;
+        case color::yellow: c = 4; break;
+        case color::blue: c = 5; break;
+        default: break;
+      }
+      
+      _flushCont+=c;
+    }
 
-                void write(color col)
-                {
-                        char c = 1;
-                        switch(col)
-                        {
-                                case color::white: c = 1;break;
-                                case color::red: c = 2; break;
-                                case color::green: c = 3; break;
-                                case color::yellow: c = 4; break;
-                                case color::blue: c = 5; break;
-                                default: break;
-                        }
-                        
-                        _flushCont+=c;
-                }
-
-                void flush()
-                {
-                        s->send(_conn,_flushCont,websocketpp::frame::opcode::value::text);
-                        _flushCont = "";
-                }
+    void flush() {
+      s->send(_conn,_flushCont,websocketpp::frame::opcode::value::text);
+      _flushCont = "";
+    }
 };
 
--- Webgame.hpp
+++ Webgame.hpp
@@ -20,143 +20,135 @@ namespace asio = websocketpp::lib::asio;
 typedef websocketpp::lib::shared_ptr<websocketpp::lib::asio::ssl::context> context_ptr;
 #endif
 
-    template<typename T>
-void sig_handler(int)
-{
-    delete (T)gWebgameAddr;
+template<typename T>
+void sig_handler(int) {
+  delete (T)gWebgameAddr;
 }
 
 inline std::string get_password() {
-    return "password";
+  return "password";
 }
 
 template<typename GameType>
 class Webgame
 {
-    public:
-	std::mutex ml;
-	std::map<decltype(websocketpp::lib::weak_ptr<void>().lock().get()),GameType*> mp;
-	server echo_server;
+  public:
+    std::mutex ml;
+    std::map<decltype(websocketpp::lib::weak_ptr<void>().lock().get()),GameType*> mp;
+    server echo_server;
 
-	Webgame()
-	{
-	    signal(SIGTERM, sig_handler<Webgame<GameType>*>);
-	}
+    Webgame() {
+      signal(SIGTERM, sig_handler<Webgame<GameType>*>);
+    }
 
-	~Webgame()
-	{
-	    echo_server.stop();
-	}
+    ~Webgame() {
+      echo_server.stop();
+    }
 
-#ifdef _COMPILE_FOR_SERVER_
-	context_ptr on_tls_init(websocketpp::connection_hdl hdl) {
-	    context_ptr ctx = websocketpp::lib::make_shared<asio::ssl::context>(asio::ssl::context::sslv23);
+    #ifdef _COMPILE_FOR_SERVER_
+	  context_ptr on_tls_init(websocketpp::connection_hdl hdl) {
+	    context_ptr ctx = websocketpp::lib::make_shared<asio::ssl::context>
+        (asio::ssl::context::sslv23);
 	    try {
 		    ctx->set_options(asio::ssl::context::default_workarounds |
 			    asio::ssl::context::no_sslv2 |
 			    asio::ssl::context::no_sslv3 |
 			    asio::ssl::context::no_tlsv1 |
 			    asio::ssl::context::single_dh_use);
-		ctx->set_password_callback(bind(&get_password));
-		ctx->use_certificate_chain_file("/etc/letsencrypt/live/fkaf.eu/fullchain.pem");
-		ctx->use_private_key_file("/etc/letsencrypt/live/fkaf.eu/privkey.pem", asio::ssl::context::pem);
-		ctx->use_tmp_dh_file("/etc/letsencrypt/ssl-dhparams.pem");
-		// Example method of generating this file:
-		// `openssl dhparam -out dh.pem 2048`
-		// Mozilla Intermediate suggests 1024 as the minimum size to use
-		// Mozilla Modern suggests 2048 as the minimum size to use.
+		    ctx->set_password_callback(bind(&get_password));
+        ctx->use_certificate_chain_file("/etc/letsencrypt/live/fkaf.eu/fullchain.pem");
+        ctx->use_private_key_file("/etc/letsencrypt/live/fkaf.eu/privkey.pem", 
+            asio::ssl::context::pem);
+        ctx->use_tmp_dh_file("/etc/letsencrypt/ssl-dhparams.pem");
+        // Example method of generating this file:
+        // `openssl dhparam -out dh.pem 2048`
+        // Mozilla Intermediate suggests 1024 as the minimum size to use
+        // Mozilla Modern suggests 2048 as the minimum size to use.
 
-		std::string ciphers;
-		ciphers = "ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS";
-		if (SSL_CTX_set_cipher_list(ctx->native_handle() , ciphers.c_str()) != 1) {
-		    std::cout << "Error setting cipher list" << std::endl;
-		}
-	    } catch (std::exception& e) {
-		std::cout << "Exception: " << e.what() << std::endl;
-	    }
-	    return ctx;
-	}
-#endif
-	void run()
-	{
+        std::string ciphers;
+        ciphers = "ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS";
+        if (SSL_CTX_set_cipher_list(ctx->native_handle() , ciphers.c_str()) != 1) {
+            std::cout << "Error setting cipher list" << std::endl;
+        }
+      } 
+      catch (std::exception& e) {
+        std::cout << "Exception: " << e.what() << std::endl;
+      }
+      return ctx;
+	  }
+    #endif
 
+	  void run() {
 	    try {
-		// Set logging settings
-		echo_server.set_access_channels(websocketpp::log::alevel::all);
-		echo_server.clear_access_channels(websocketpp::log::alevel::frame_payload);
+        // Set logging settings
+        echo_server.set_access_channels(websocketpp::log::alevel::all);
+        echo_server.clear_access_channels(websocketpp::log::alevel::frame_payload);
 
-		// Initialize Asio
-		echo_server.init_asio();
+        // Initialize Asio
+        echo_server.init_asio();
 
-		// Register our message handler
-		echo_server.set_message_handler(bind(&Webgame<GameType>::on_message,this,&echo_server,::_1,::_2));
+        // Register our message handler
+        echo_server.set_message_handler(bind(&Webgame<GameType>::on_message,this,&echo_server,::_1,::_2));
 #ifdef _COMPILE_FOR_SERVER_
-		echo_server.set_tls_init_handler(bind(&Webgame<GameType>::on_tls_init,this,::_1));
+        echo_server.set_tls_init_handler(bind(&Webgame<GameType>::on_tls_init,this,::_1));
 #endif
-		echo_server.set_open_handler(bind(&Webgame<GameType>::on_open,this,::_1));
-		echo_server.set_close_handler(bind(&Webgame<GameType>::on_open,this,::_1));
-		echo_server.set_reuse_addr(true);
+        echo_server.set_open_handler(bind(&Webgame<GameType>::on_open,this,::_1));
+        echo_server.set_close_handler(bind(&Webgame<GameType>::on_close,this,::_1));
+        echo_server.set_reuse_addr(true);
 
-		// Listen on port 9002
-		echo_server.listen(9002);
+        // Listen on port 9002
+        echo_server.listen(9002);
 
-		// Start the server accept loop
-		echo_server.start_accept();
+        // Start the server accept loop
+        echo_server.start_accept();
 
-		// Start the ASIO io_service run loop
-		echo_server.run();
-	    } catch (websocketpp::exception const & e) {
-		std::cout << e.what() << std::endl;
-	    } catch (...) {
-		std::cout << "other exception" << std::endl;
-	    }
-	}
+        // Start the ASIO io_service run loop
+        echo_server.run();
+      } 
+      catch (websocketpp::exception const & e) {
+        std::cout << e.what() << std::endl;
+      } 
+      catch (...) {
+        std::cout << "other exception" << std::endl;
+      }
+	  }
 
-	void on_close(websocketpp::connection_hdl hdl)
-	{
-	    std::lock_guard lk(ml);
-	    try
-	    {
-		auto k = mp.at(hdl.lock().get());
-		delete k;
-		if(mp.size()>1)
-		    mp.erase(hdl.lock().get());
-		else
-		    mp.clear();
-	    }
-	    catch(...)
-	    {
-	    }
-	}
+    void on_close(websocketpp::connection_hdl hdl) {
+      std::lock_guard lk(ml);
+      try {
+        auto k = mp.at(hdl.lock().get());
+        delete k;
+        if(mp.size()>1)
+          mp.erase(hdl.lock().get());
+        else
+          mp.clear();
+      }
+      catch(...) {}
+    }
 
-	void on_open(websocketpp::connection_hdl hdl)
-	{
-	    {
-		std::lock_guard lk(ml);
-		try
-		{
-		    mp.at(hdl.lock().get());
-		    return;
-		}
-		catch(...)
-		{
-		    mp[hdl.lock().get()] = new GameType(new Webconsole(&echo_server,hdl));
-		}
-	    }
-	}
+    void on_open(websocketpp::connection_hdl hdl) {
+      {
+        std::lock_guard lk(ml);
+        try {
+          mp.at(hdl.lock().get());
+          return;
+        }
+        catch(...) {
+          mp[hdl.lock().get()] = new GameType(new Webconsole(&echo_server,hdl));
+        }
+      }
+    }
 
-	void on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg) {
-	    {
-		std::lock_guard lk(ml);
-		try
-		{
-		    auto k = mp.at(hdl.lock().get());
-		    k->onmessage(msg->get_payload(),&mp);
-		}
-		catch(...)
-		{
-		    return;
-		}
-	    }
-	}
+    void on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg) {
+      {
+        std::lock_guard lk(ml);
+        try {
+          auto k = mp.at(hdl.lock().get());
+          k->onmessage(msg->get_payload(),&mp);
+        }
+        catch(...) {
+          return;
+        }
+      }
+    }
 };
